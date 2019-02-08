ac3965f4a5d1a87f2c70e7134d32579e82856f4e
grantSwalwell <grant.swalwell-1@ou.edu>
2019-02-07 21:39:43 -0600

    Saving Matrix progress

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -1,20 +1,15 @@
 #pragma once
 #include <iostream>
-#include "Array\Array.h"
+#include "C:\Users\Grant\VS\Data-Structures\Array\Array.h"
 // matrix class
 
-template<class x> class Matrix
+template<class X> class Matrix
 {
 
 protected:
 	
 	// array of arrays, rows
-	Array<Array<x>*>* rows;
-
-public:
-
-	// number of rows * number of clumns
-	int size;
+	Array<Array<X>*>* mat;
 
 	// number of rows
 	int height;
@@ -25,51 +20,76 @@ public:
 	// print function for ostream
 	ostream& print (ostream& os)
 	{
-		for (int i = 0; i < rows->size; i++) os << (*(*rows) [i]) << endl;
+		for (int i = 0; i < height; i++) os << (*(*mat)[i]) << endl;
 
 		return os;
 	};
 
+public:
+
+	int Height() { return height; };
+
+	int Width() { return width; };
+
 	// ostream output override
 	friend ostream& operator<< (ostream& os, Matrix& mat)
 	{
-		return mat.print (os);
+		return mat.print(os);
 	};
-
 
 	// [] operator override
-	Array<x>& operator[] (int index)
+	Array<X>& operator[] (int index)
 	{
-		// retrieve row at index
-		if (index >= 0 && index < rows->size) return (*(*rows) [index]);
+		return (*(*mat)[index]);
 	};
 
+	Matrix& operator=(Matrix& mat)
+	{
+		height = mat.Height();
+		width = mat.Width();
+
+		if (this->mat != NULL) delete this->mat;
+
+		this->mat = new Array<Array<X>*>(height);
+
+		for (int i = 0; i < height; i++)
+		{
+			(*this->mat)[i] = new Array<X>(width);
+
+			for (int j = 0; j < width; j++)
+			{
+				(*(*this->mat)[i])[j] = mat[i][j];
+			}
+		}
+
+		return *this;
+	}
+
 	// default constructor
-	Matrix () { rows = NULL; };
+	Matrix () 
+	{ 
+		height = 0;
+		width = 0;
+		mat = NULL;
+	};
 
 	// initializer
 	Matrix (int size)
 	{
-		// set size
-		this->size = size * size;
-
 		// set height and width
 		height = size;
 		width = size;
 
 		// set rows to be array size of arrays
-		rows = new Array<Array<x>*> (size);
+		mat = new Array<Array<X>*> (size);
 
 		// initialize each row
-		for (int i = 0; i < size; i++) (*rows) [i] = new Array<x> (size);
+		for (int i = 0; i < size; i++) (*mat)[i] = new Array<X> (size);
 	};
 
 	// initializer
 	Matrix (int rows, int columns) 
 	{
-		// set size
-		this->size = rows * columns;
-
 		// set height
 		height = rows;
 
@@ -77,17 +97,54 @@ public:
 		width = columns;
 
 		// initialize rows
-		this->rows = new Array<Array<x>*> (rows);
+		this->mat = new Array<Array<X>*> (rows);
 
 		// initialize each row
-		for (int i = 0; i < rows; i++) (*this->rows) [i] = new Array<x> (columns);
+		for (int i = 0; i < rows; i++) (*this->mat) [i] = new Array<X> (columns);
+	};
+
+	// initializer
+	Matrix (int rows, int columns, const X& val)
+	{
+		// set height
+		height = rows;
+
+		// set width
+		width = columns;
+
+		// initialize rows
+		this->mat = new Array<Array<X>*> (rows);
+
+		// initialize each row
+		for (int i = 0; i < rows; i++) (*this->mat)[i] = new Array<X>(columns, val);
 	};
 
 	// copy constructor 
-	Matrix (Matrix& mat) {};
+	Matrix (Matrix& mat)
+	{
+		height = mat.Height();
+		width = mat.Width();
+
+		this->mat = new Array<Array<X>*>(height);
+
+		for (int i = 0; i < height; i++)
+		{
+			(*this->mat)[i] = new Array<X>(width);
+
+			for (int j = 0; j < width; j++)
+			{
+				(*(*this->mat)[i])[j] = mat[i][j];
+			}
+		}
+	};
 
 	// destructor
-	~Matrix () { delete rows; };
-
+	~Matrix ()
+	{
+		if (mat != NULL)
+		{
+			delete mat;
+		}
+	};
 
 };
